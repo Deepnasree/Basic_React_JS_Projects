1f04e3335576ded329b25b1b852e3d55215b9898
Deepnasree <deepnabachu@gmail.com>
2024-12-26 12:38:55 +0530

    App Comp

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,7 +1,15 @@
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+
 function App() {
   return(
+    <div>
   <h1>Hello From React JS</h1>
-  
+    <Home />
+    <About />
+    <Contact />
+    </div>
   );
 }
 
---
+++ basics/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return(
+        <>
+        <h1>Header Component</h1>
+        <header>Some header Content</header>
+        </>
+    );
+}
+export default Header;
